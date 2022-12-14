ff1c0e62cb54cc7fc6206579d9fc770ba3f2b09a
Stefan <stefanhulspas@gmail.com>
2022-12-14 12:46:45 +0100

    Day4

---
+++ Day4/Part1.py
@@ -0,0 +1,22 @@
+f = open("Day4/input.txt", "r")
+Lines = f.readlines()
+
+def contains(first,second):
+    firstSplit = first.split('-')
+    secondSplit = second.split('-') 
+    if int(firstSplit[0]) <= int(secondSplit[0]) and int(firstSplit[1]) >= int(secondSplit[0]):
+        if int(firstSplit[0]) <= int(secondSplit[1]) and int(firstSplit[1]) >= int(secondSplit[1]):
+            return True
+    return False
+
+score = 0
+for line in Lines:
+    line = line.replace("\n", "")
+    split = line.split(',')
+    s = line
+    if contains(split[0], split[1]):
+        score += 1
+    elif contains(split[1], split[0]):
+        score += 1
+
+print("Sections score: {}".format(score))
